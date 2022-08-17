a1207a0bc4c93f17c54aa1f068da4898a41e8606
Abdel Hak <user@users-MBP.home>
2022-08-17 09:43:21 +0100

    update1

---
+++ api1/querd.py
@@ -0,0 +1,16 @@
+from random import*
+import time
+
+
+test = True
+while test == True:
+    lat = uniform(-90.0, 90.0)
+    lng = uniform(-180.0, 180.0)
+    speed = randint(0,100)
+    sat = randint(0,10)	
+    with open("coordinates.txt", "w") as data:
+        data.write(f"{lat}\n{lng}\n{speed}\n{sat}\n")
+    time.sleep(10)
+
+
+
